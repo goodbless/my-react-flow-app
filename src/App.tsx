4442f6a9c223c53b7b3464bb7aa9ec9c5abdfb89
goodbless <evenGodnotknow@hotmail.com>
2025-12-07 12:57:15 +0800

    feat: import basci node ui & create num-node

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-export function App() {
+import { ReactFlow, useNodesState, type Node } from "@xyflow/react";
+ 
+import "@xyflow/react/dist/style.css";
+import { NumNode } from "./components/nodes/num-node";
+
+
+ 
+const nodeTypes = {
+  num: NumNode,
+};
+ 
+const initialNodes: Node[] = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { value: 42 },
+    type: "num",
+  },
+];
+ 
+function Flow() {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+ 
   return (
-    <div className="w-screen h-screen p-8">
-      <h1 className="text-2xl font-bold">Hello World</h1>
+    <div className="h-screen w-screen p-8 bg-gray-50 rounded-xl">
+      <ReactFlow
+        nodes={nodes}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        fitView
+      />
     </div>
   );
 }
-export default App;
+ 
+export default function App() {
+  return <Flow />;
+}
